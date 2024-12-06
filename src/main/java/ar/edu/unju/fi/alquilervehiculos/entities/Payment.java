1facbb764d0b9e8c82990efd34ef6681ef392148
CristianT2 <c.torrejon96@gmail.com>
2024-12-05 22:52:37 -0300

    Se crearosn las entidades Rent y Payment y sus entidades relacionadas

---
+++ src/main/java/ar/edu/unju/fi/alquilervehiculos/entities/Payment.java
@@ -0,0 +1,41 @@
+package ar.edu.unju.fi.alquilervehiculos.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "rent_id")
+    private Rent rent;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
+
+    private LocalDateTime date;
+    private double amount;
+
+    @ManyToOne
+    @JoinColumn(name = "payment_method_id")
+    private PaymentMethod paymentMethod;
+}
---
+++ src/main/java/ar/edu/unju/fi/alquilervehiculos/entities/Rent.java
@@ -0,0 +1,45 @@
+package ar.edu.unju.fi.alquilervehiculos.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "rents")
+public class Rent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "state_rent_id")
+    private StateRent stateRent;
+}
